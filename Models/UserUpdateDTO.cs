f471a0868e8db1979aa45aaf9c2c3cfd151f4bf0
Carlota Cetina <carlotika26@gmail.com>
2024-11-20 19:10:54 +0100

    Removed required password validation in UpdateDTO and updated the service to only update the password if it is provided.

--- Models/UserUpdateDTO.cs
+++ Models/UserUpdateDTO.cs
@@ -7,8 +7,6 @@ public class UserUpdateDTO
     [Required(ErrorMessage = "El campo de usuario es obligatorio.")]
     public string? UserName { get; set; }
 
-    [Required(ErrorMessage = "El campo de contraseña es obligatorio.")]
-    [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
     public string? Password { get; set; }
 
     [Required(ErrorMessage = "El campo de correo electrónico es obligatorio.")]
